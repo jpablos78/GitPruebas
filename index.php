2552426ef5bfa72e5245a25d41e008a82727a1e3
jpsanchez <jpsanchez@JPSANCHEZ.inti-moda.com>
2017-03-14 12:39:09 -0500

    Version 2

--- index.php
+++ index.php
@@ -12,6 +12,8 @@ and open the template in the editor.
     <body>
         <?php
         echo 'Pruebas de Github';
+        
+        echo '</br>Mas cambios en local';
         ?>
     </body>
 </html>
